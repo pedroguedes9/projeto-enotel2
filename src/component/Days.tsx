82abf04627cd3a2c414c7319a489381fcb1b74ca
Pedro Chaves Guedes <pedro.guedes.mcz@gmail.com>
2025-11-20 00:03:59 -0300

    Adicionei as fotos de fundo e algumas estilizações

--- src/component/Days.tsx
+++ src/component/Days.tsx
@@ -1,10 +1,20 @@
 type Props = {
     imageSrc: string
+    type?: "corner-top" | "corner-bottom"
+}
+const type = {
+    cornerTop: "rounded-t-xl" ,
+    cornerBottom: "rounded-b-xl" 
 }
 const Days = (props: Props) => {
     return (
-        <div className={`bg-[url('${props.imageSrc}')] h-24 w-24`}>
-            Oi
+        <div
+            className={`max-w-5xl h-screen bg-center mx-auto snap-center bg-cover bg-no-repeat border-2 shadow-black shadow-xl bg-fixed 
+                    ${props.type === "corner-top" && type.cornerTop}
+                    ${props.type === "corner-bottom" && type.cornerBottom}
+                `}
+            style={{backgroundImage: `url(${props.imageSrc})`}}
+        >
         </div>
     )
 }
