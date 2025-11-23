648178d4b4454f965f3792cebf6771f38789a0d6
Pedro Chaves Guedes <pedro.guedes.mcz@gmail.com>
2025-11-23 10:35:27 -0300

    to ajustando o metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { title } from "process";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -15,6 +16,25 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Contador do Enotel",
   description: "ENOTEL TA CHEGANDO",
+  icons: {
+    icon: "/favicon.ico"
+  },
+  openGraph: {
+    type: "website",
+    title: "Contador do Enotel",
+    description: "ENOTEL TA CHEGANDO",
+    url: "https://enotel.vercel.app",
+    siteName: "Contador do Enotel",
+    images: [
+      {
+        url: "/favicon.ico",
+        width: 1200,
+        height: 630,
+        alt: "Enotel",
+      },
+    ],
+    locale: "pt_BR",
+  }
 };
 
 export default function RootLayout({
